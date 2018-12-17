4e2d8849283a07cec6e76942eb37f11a54ec9362
taching <taching@aliyun.com>
2018-12-17 11:45:59 +0800

    增加nav

--- AwesomeProject/src/pages/index.js
+++ AwesomeProject/src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 class DetailsScreen extends React.Component {
@@ -16,6 +16,10 @@ class HomeScreen extends React.Component {
         return (
             <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
                 <Text>Home Screen</Text>
+                <Button
+                    title="Go to Details"
+                    onPress={() => this.props.navigation.navigate('Details')}
+                />
             </View>);
     }
 }
