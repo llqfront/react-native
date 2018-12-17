f74b32a7d52efb49d409caa06204eb30e455b524
taching <taching@aliyun.com>
2018-12-17 12:05:25 +0800

    增加nav

--- AwesomeProject/src/pages/index.js
+++ AwesomeProject/src/pages/index.js
@@ -1,33 +1,7 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from "react-navigation";
 
-class DetailsScreen extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <Text>Details Screen</Text>
-                <Button
-                    title="Go to Details... again"
-                    // onPress={() => this.props.navigation.navigate('Details')} // 不跳转
-                    onPress={() => this.props.navigation.push('Details')} // 同一路由跳转
-                />
-                <Button
-                    title="Go to Details... again"
-                    onPress={() => this.props.navigation.push('Details')}
-                />
-                <Button
-                    title="Go to Home"
-                    onPress={() => this.props.navigation.navigate('Home')}
-                />
-                <Button
-                    title="Go back"
-                    onPress={() => this.props.navigation.goBack()}
-                />
-            </View>
-        );
-    }
-}
 class HomeScreen extends React.Component {
     render() {
         return (
@@ -40,11 +14,70 @@ class HomeScreen extends React.Component {
             </View>);
     }
 }
+class DetailsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Details Screen</Text>
+                {/* <Button
+                    title="Go to Details... again"
+                    // onPress={() => this.props.navigation.navigate('Details')} // 不跳转
+                    onPress={() => this.props.navigation.push('Details')} // 同一路由跳转
+                /> */}
+                <Button
+                    title="Go to Details... again"
+                    onPress={() => this.props.navigation.push('Details')}
+                />
+                <Button
+                    title="Go to Home"
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
+                <Button
+                    title="Go back"
+                    onPress={() => this.props.navigation.goBack()}
+                />
 
-const AppNavigator = createStackNavigator(
+                <Button
+                    title="Go to Settings"
+                    onPress={() => this.props.navigation.push('Settings')}
+                />
+                <Button
+                    title="Go to Profile"
+                    onPress={() => this.props.navigation.push('Profile')}
+                />
+            </View>
+        );
+    }
+}
+class SettingsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>SettingsScreen</Text>
+            </View>);
+    }
+}
+class ProfileScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>ProfileScreen</Text>
+            </View>);
+    }
+}
+
+const HomeStack = createStackNavigator({
+    Home: HomeScreen,
+    Details: DetailsScreen,
+});
+const SettingsStack = createStackNavigator({
+    Settings: SettingsScreen,
+    Profile: ProfileScreen,
+});
+const AppNavigator = createBottomTabNavigator(
     {
-        Home: HomeScreen,
-        Details: DetailsScreen
+        Home: HomeStack,
+        Details: SettingsStack
     },
     {
         initialRouteName: "Home"
